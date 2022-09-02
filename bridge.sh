6542f1d94ba3153cb1be577d6ec40398e6bd6b0b
uidops <uidops@protonmail.com>
2022-09-03 00:35:58 +0430

    new version with a better code

--- bridge.sh
+++ bridge.sh
@@ -12,41 +12,23 @@ else
 fi
 
 get_request=$(curl -X GET -s $url)
-field=$(echo $get_request | tr " " "\n" | grep 'value=\"' | sed -e 's/value="/\ /;s/" /\ /')
-
-word='</input>'
-word2='"'
-word3=">"
-word4="submit"
-word5=" "
-
-a=$(echo "${field//$word}")
-b=$(echo "${a//$word2}")
-b=$(echo "${b//$word3}")
-b=$(echo "${b//$word4}")
-key=$(echo "${b//$word5}")
-field=$(echo $get_request | tr " " "\n" | grep 'src="data:image/jpeg;base64')
-a=$(echo "${field//$word2}")
-
-word="src=data"
-word1=":"
-word2="image/"
-word4="jpeg;"
-word6="base64,"
-
-a=$(echo "${a//$word}")
-a=$(echo "${a//$word1}")
-a=$(echo "${a//$word2}")
-a=$(echo "${a//$word4}")
-img=$(echo "${a//$word6}")
+key=$(echo $get_request | tr " " "\n" | grep 'value=\"' | sed -e 's/value="/\ /;s/" /\ /;s/^ //;s/">//')
+img=$(echo $get_request | tr " " "\n" | grep 'src="data:image/jpeg;base64' | sed -e 's/src="data:image\/jpeg;base64,//;s/">$//')
 
 echo $img > /tmp/torbridge.txt
 base64 -d /tmp/torbridge.txt > /tmp/torbridge.jpg
 
-feh /tmp/torbridge.jpg & > /dev/null
+feh /tmp/torbridge.jpg & >/dev/null 2>&1
 
 printf "code: "
 read code
 post_request=$(curl -X POST -s $url -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -d "captcha_challenge_field=${key}&captcha_response_field=${code}&submit=submit")
-echo
-echo $post_request | pup 'div.bridge-lines text{}'
+
+if printf '%s' "${post_request}" | head -10 | grep 'click here' | grep 'bridges?' > /dev/null; then
+    echo "Invalid code"
+    exit 1
+else
+    echo "$(printf '%s' "$post_request" | tr '\n' ' ' | sed -e 's/.*<div id="bridgelines"//;s/<\/div>.*//;s/.*"> //;s/<br \/>/\n/g' | sed -e 's/^ *//g')"
+fi
+
+exit 0
